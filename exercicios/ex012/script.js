c5195d1665c1006364464b3a65909f75c9e216ff
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-06-05 09:13:01 -0300

    Update script.js

--- exercicios/ex012/script.js
+++ exercicios/ex012/script.js
@@ -1,1 +1,21 @@
+// DECLARING VARIABLES IN A SINGLE LINE
 
+// let user = 'John', age = 25, message = 'Hello';
+
+// DECLARING VARIABLES ON MULTIPLE LINES
+
+let user = 'John';
+let age = 25;
+let message = 'Hello';
+
+// ANOTHER WAY IN MULTIPLE LINES
+
+/* let user = 'John',
+  age = 25,
+  message = 'Hello'; */
+
+// ANOTHER WAY IN MULTIPLE LINES
+
+/* let user = 'John'
+  , age = 25
+  , message = 'Hello'; */
