e74d34e9ed5e86ac3e36d7466287d02a529b9c74
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-06-05 10:27:24 -0300

    Create script.js with birthday and age guidelines
    Add initial script.js with birthday and age comments

---
+++ desafios/D005/script.js
@@ -0,0 +1,10 @@
+/* const birthday = '18.04.1982';
+
+const age = someCode(birthday);
+
+const BIRTHDAY = '18.04.1982'; // make birthday uppercase?
+const AGE = someCode(BIRTHDAY); // make age uppercase? */
+
+// SOLUTION
+// const BIRTHDAY should be in uppercase
+// const age should be in lowercase
