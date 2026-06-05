f6792f61fa63203df749f759c163ee2443a50eaa
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-06-05 09:41:25 -0300

    Create script.js for variable declaration examples
    Added a script demonstrating variable declarations and naming rules in JavaScript.

---
+++ exercicios/ex017/script.js
@@ -0,0 +1,20 @@
+let userName;
+let test123;
+
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a varible with the name "_"
+
+alert($ + _); // 3
+
+// let 1a;
+// cannot start with a digit
+// let my-name;
+// hyphens '-' aren't allowed in the name
+
+let имя = '...';
+let 我 = '...';
+
+// let let = 5;
+// can't name a variable "let", error!
+// let return = 5;
+// also can't name it "return", error!
