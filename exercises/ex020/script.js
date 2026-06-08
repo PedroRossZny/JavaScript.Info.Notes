0c2c18caa250514673138619e245453c2a80097b
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-06-08 15:03:29 -0300

    Add script.js with various number operations

---
+++ exercises/ex020/script.js
@@ -0,0 +1,14 @@
+// no error
+let message = "hello";
+message = 123456;
+
+let n = 123;
+n = 12.345;
+
+alert( 1 / 0); // Infinity
+
+alert( Infinity ); // Infinity
+
+alert( NaN + 1); // NaN
+alert( 3 * NaN); // NaN
+alert( "not a number" / 2 - 1 ); // NaN, such division is erroneous
