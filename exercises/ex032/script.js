3f1582c49f9ca98b4e00a16266b780fb0eb2fbab
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-06-10 14:35:47 -0300

    Add script.js with variable assignments and alerts

---
+++ exercises/ex032/script.js
@@ -0,0 +1,23 @@
+let x = 2 * 2 + 1;
+
+alert( x ); // 5
+
+let a = 1;
+let b = 2;
+
+let c = 3 - (a = b + 1);
+
+alert( a ); // 3
+alert( c ); // 0
+
+let a, b, c;
+
+a = b = c = 2 + 2;
+
+alert( a ); // 4
+alert( b ); // 4
+alert( c ); // 4
+
+c = 2 + 2;
+b = c;
+a = c;
